80acd3a9730405dd1d86a3642aa34c1a6e7a0fff
mincongtran1000 <mincongtran1804@gmail.com>
2024-03-12 21:50:59 +0700

    first commit

---
+++ app/api-error.js
@@ -0,0 +1,8 @@
+class ApiError extends Error {
+    constructor(statusCode, message){
+        super();
+        this.status = statusCode;
+        this.message = message;
+    }
+}
+module.exports = ApiError;
